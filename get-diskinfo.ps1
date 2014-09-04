e29bde87daa6bc68b73006cb0b6ae668a671ef83
Brendan Cann <brendan.cann@gmail.com>
2014-09-04 21:12:37 +1000

    Create get-diskinfo.ps1
    Gets the size of physically attached disks and lists the free space and status of the disk as ok if free space is >10%

---
+++ get-diskinfo.ps1
@@ -0,0 +1,21 @@
+Function Get-DiskInfo {
+	Param (
+		[string]$computername='localhost',
+
+		[int]$MinimumFreePercent=10
+	)
+
+	$disks=Get-WmiObject -Class Win32_Logicaldisk -Filter "Drivetype=3"
+	foreach ($disk in $disks) {
+		$perFree=($disk.FreeSpace/$disk.Size)*100
+		if ($perFree -ge $MinimumFreePercent) {
+			$OK=$True
+			}
+		else {
+		$OK=$False
+		}	
+	$disk | Select DeviceID,VolumeName,Size,FreeSpace,@{Name="OK";Expression={$OK}}
+
+	} #close foreach
+} #close function
+Get-DiskInfo -computername server1
